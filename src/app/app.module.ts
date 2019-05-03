f57b5abe5b467db1e55db00f489d0ad7f5625bbd
xystus ngigi <xystusngigi@gmail.com>
2019-05-03 11:47:05 +0300

    changes made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
+import { PageErrorComponent } from './page-error/page-error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    PageErrorComponent
   ],
   imports: [
     BrowserModule,
