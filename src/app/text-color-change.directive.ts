0fe8578943ae838f7e545841fe0d0fca865f4d69
xystus ngigi <xystusngigi@gmail.com>
2019-05-06 17:54:02 +0300

    changes made in text-color

--- src/app/text-color-change.directive.ts
+++ src/app/text-color-change.directive.ts
@@ -3,7 +3,7 @@ import { Directive, ElementRef } from '@angular/core';
 @Directive({
   selector: '[appTextColorChange]'
 })
-export TextColorChangeDirective {
+export class TextColorChangeDirective {
 
   constructor(private el: ElementRef) {
     this.el.nativeElement.style.color = '#1071ED';
